6e7a880ef3bac76cc90415894b30eb34865e27ae
Sylvain Boureliou <sylvain.boureliou@gmail.com>
2012-09-27 16:17:13 +0200

    Plone 4.0 is no longer supported

--- src/collective/z3cform/norobots/testing.py
+++ src/collective/z3cform/norobots/testing.py
@@ -16,12 +16,6 @@ class NorobotsSandboxLayer(PloneSandboxLayer):
     def setUpZope(self, app, configurationContext):
         # Load ZCML, install the products and call its initialize() function 
         
-        if PLONE_VERSION == 4.0:
-            # plone.app.registry is not include in Plone 4.0 core
-            import plone.app.registry
-            self.loadZCML(package=plone.app.registry)
-            z2.installProduct(app, 'plone.app.registry')
-        
         # Load ZCML for this package
         import collective.z3cform.norobots
         self.loadZCML(package=collective.z3cform.norobots)
@@ -31,30 +25,16 @@ class NorobotsSandboxLayer(PloneSandboxLayer):
         # Configure the products using the Quick Installer tool 
         portal_quickinstaller = getToolByName(portal, 'portal_quickinstaller')
         
-        if PLONE_VERSION == 4.0:
-            # plone.app.registry is not include in Plone 4.0 core
-            portal_quickinstaller.installProducts( ('plone.app.registry',) )
-        
         portal_quickinstaller.installProducts( ('collective.z3cform.norobots',) )
 
     def tearDownZope(self, app):
         # Uninstall products
-        
         z2.uninstallProduct(app, 'collective.z3cform.norobots')
-        
-        if PLONE_VERSION == 4.0:
-            # plone.app.registry is not include in Plone 4.0 core
-            z2.uninstallProduct(app, 'plone.app.registry')
 
     def tearDownPloneSite(self, portal):
         # Unconfigure the products using the Quick Installer tool
         portal_quickinstaller = getToolByName(portal, 'portal_quickinstaller')
-        
         portal_quickinstaller.uninstallProducts( ('collective.z3cform.norobots',) )
-        
-        if PLONE_VERSION == 4.0:
-            # plone.app.registry is not include in Plone 4.0 core        
-            portal_quickinstaller.uninstallProducts( ('plone.app.registry',) )
 
 NOROBOTS_FIXTURE = NorobotsSandboxLayer()
 NOROBOTS_INTEGRATION_TESTING = \
