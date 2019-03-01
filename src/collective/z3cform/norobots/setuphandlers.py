714709581be0be6f90b4089b09e6c5518b138986
Jan Buruck <1letter@gmx.de>
2019-03-01 10:53:03 +0100

    Add uniinstall Profile
    Move default values to registry.xml

---
+++ src/collective/z3cform/norobots/setuphandlers.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from Products.CMFPlone.interfaces import INonInstallable
+from zope.interface import implementer
+
+@implementer(INonInstallable)
+class HiddenProfiles(object):
+
+    def getNonInstallableProfiles(self):
+        """Hide uninstall profile from site-creation and quickinstaller"""
+        return [
+            'collective.z3cform.norobots:uninstall',
+        ]
+
+def uninstall(context):
+    """Uninstall script"""
+    # Do something at the end of the uninstallation of this package.
