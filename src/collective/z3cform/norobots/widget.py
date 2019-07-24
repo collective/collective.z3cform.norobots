f3170903633eb5d36f40c6d29b946aacb34a4da3
1letter <1letter@gmx.de>
2019-07-24 11:10:27 +0200

    Add missing Interface on Widget Defintion
    - Refactoring Widget for work with plone.autoforms.directives
    
    Example:
    
    Add the Widget with plone.autoform.directives
    
    ```
    import from collective.z3cform.norobots.widget import NorobotsFieldWidget
    
    directives.widget('norobots', NorobotsFieldWidget)
    norobots = schema.TextLine(
        title = _(u'In order to avoid spam, please answer the question below.'),
        required = True
    )
    ```
    
    It fails with an error
    
        TypeError: widget_class must implement IWidget or IFieldWidget
    
    Now it works.

--- src/collective/z3cform/norobots/widget.py
+++ src/collective/z3cform/norobots/widget.py
@@ -1,10 +1,16 @@
 from Acquisition import aq_inner
+from zope.component import adapter
 from zope.component import getMultiAdapter
+from zope.interface import implementer
 from zope.interface import implementer_only
+from zope.schema.interfaces import IField
 
 from z3c.form.interfaces import IWidget
+from z3c.form.interfaces import IFieldWidget
+from z3c.form.interfaces import IFormLayer
 from z3c.form.widget import FieldWidget
 from z3c.form.browser.text import TextWidget
+
 
 class INorobotsWidget(IWidget):
     """Marker interface for th norobots widget
@@ -23,6 +29,8 @@ class NorobotsWidget(TextWidget):
         self.norobots = getMultiAdapter((aq_inner(self.context), self.request), name='norobots')
         return self.norobots.get_question()
 
+@adapter(IField, IFormLayer)
+@implementer(IFieldWidget)
 def NorobotsFieldWidget(field, request):
     """IFieldWidget factory for NorobotsWidget."""
     return FieldWidget(field, NorobotsWidget(request))
