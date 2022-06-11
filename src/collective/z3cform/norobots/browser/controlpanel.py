22841b41b1b1240b44b272ba0553d1eeac83d574
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-06-11 21:04:54 +0200

    trailing comma

--- src/collective/z3cform/norobots/browser/controlpanel.py
+++ src/collective/z3cform/norobots/browser/controlpanel.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-from collective.z3cform.norobots.browser.interfaces import INorobotsWidgetSettings
+from collective.z3cform.norobots.browser.interfaces import (
+    INorobotsWidgetSettings,
+)
 from collective.z3cform.norobots.i18n import norobotsMessageFactory as _
 from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
 from plone.app.registry.browser.controlpanel import RegistryEditForm
